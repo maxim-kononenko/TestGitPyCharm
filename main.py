d9e90c6fe4ef5f464256a3d1375e21fb1961d9cd
Maxim Kononenko <maxim.kononenko@sim-labs.com>
2022-09-05 17:53:08 +0300

    Revert "Lats 'Lost'"
    This reverts commit e5746543ba236a7394cf6451ee77abf68b2d06e6.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Lost')
+    print_hi('Test GitHub + PyCharm 3. Third commit')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
