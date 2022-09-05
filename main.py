f23d27bd5711590e74004ebe87426dc674c4b939
Maxim Kononenko <maxim.kononenko@sim-labs.com>
2022-09-05 18:01:12 +0300

    Commit 4

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Test GitHub + PyCharm 3. Third commit')
+    print_hi('Test 4.')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
